c0f4c040dfe68cbabc754abdf940ad22e40cb32c
weimawt <remart.abejay@urios.edu.ph>
2023-04-26 06:32:28 +0000

    push update

--- sample.c
+++ sample.c
@@ -4,5 +4,7 @@ int main() {
          printf("Hello World in Git!");
          printf("I am Remart");
          printf("lab8");
+         printf("I am Rem");
+         printf("LAB8");
          return 0;
 }
